2feafeea3088028bfe991b116eefb0dce195ce48
Odarell35 <leratomawash0@gmail.com>
2023-12-24 02:06:55 -0800

    fixing code

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,7 +5,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/',strict_slashes=False)
 def hello():
     """returns hello hbnb"""
     return 'Hello HBNB!'
@@ -13,4 +13,3 @@ def hello():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-    app.url_map.strict_slashes = False
