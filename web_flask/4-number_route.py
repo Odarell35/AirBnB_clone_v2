d12f155b495de59f0a30f633eeb1b3d15298e382
Odarell35 <leratomawash0@gmail.com>
2023-12-24 02:59:08 -0800

    number route

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""Flask web application"""
+
+from flask import Flask, url_for
+from markupsafe import escape
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """returns hello hbnb"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def display_page():
+    """display 'HBNB'"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_text(text):
+    """displays text"""
+    text = text.replace("_", " ")
+    return f'C {text}'
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def display_t(text='is cool'):
+    """displays text"""
+    text = text.replace("_", " ")
+    return f'Python {text}'
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """displays number if int"""
+    return str(n) + ' is a number'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
