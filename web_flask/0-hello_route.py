6a91ffe989803dfdd762889ea60f1d69300297a8
Odarell35 <leratomawash0@gmail.com>
2023-12-24 01:24:22 -0800

    readme file

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,9 +1,13 @@
+#1/usr/bin/python3
+"""Flask web application"""
+
 from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello():
+    """returns hello hbnb"""
     return 'Hello HBNB!'
 
 
