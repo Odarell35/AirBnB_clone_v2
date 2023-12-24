08a2f2dfb9fee73402ef3192a2dd64ae2c087744
Odarell35 <leratomawash0@gmail.com>
2023-12-24 02:47:28 -0800

    3-python_route

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Flask web application"""
+
+from flask import Flask, url_for
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """returns hello hbnb"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def display_page():
+    """display 'HBNB'"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_text(text):
+    """displays text"""
+    text = text.replace("_", " ")
+    return f'C {text}'
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def display_t(text='is cool'):
+    """displays text"""
+    text = text.replace("_", " ")
+    return f'Python {text}'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
